feb58035a7b14293196b3fd010914b0ca8c0fe67
cristianfchq <cristian.fchq@gmail.com>
2021-02-19 00:24:52 -0400

    add the constructor into the class Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,11 @@
 public class Calculator {
-    
+
+    int numberOne;
+    int numberTwo;
+
+    public Calculator(int numberOne, int numberTwo) {
+        this.numberOne = numberOne;
+        this.numberTwo = numberTwo;
+    }
+
 }
